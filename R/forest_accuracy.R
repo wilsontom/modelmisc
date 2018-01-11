392e2caf4806dd556b4b68aec7ac45471443ea78
wilsontom <wilsontom89@gmail.com>
2018-01-11 16:06:21 +0000

    forest accuracy

---
+++ R/forest_accuracy.R
@@ -0,0 +1,14 @@
+#' Forest Accuracy
+#'
+#' Retrieve the forest accuracy (1 - OOB)
+#'
+#' @param model a randomForest model object
+#' @return a numeric value for the model accuracy (0 - 1)
+#'
+#' @export
+
+forest_accuracy <- function(model)
+{
+  oob <- model$err.rate
+  return(1 - as.numeric(oob[nrow(oob),"OOB"][[1]]))
+}
